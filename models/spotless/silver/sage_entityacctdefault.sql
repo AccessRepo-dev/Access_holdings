e90933731e0789a11ad689d3fab7655c64aa03ea
Lahari Yadav <lahari.yadav@accordion.com>
2025-09-02 11:36:32 +0000

    added spotless sage scripts

--- models/spotless/silver/sage_entityacctdefault.sql
+++ models/spotless/silver/sage_entityacctdefault.sql
@@ -6,21 +6,13 @@
 
 SELECT
     -- Primary Key
-    UPPER(TRIM(ENTITYID)) AS ENTITY_ID,
-
+    TRIM(ENTITYID) AS ENTITY_ID,
     -- Core Identifiers
     TRIM(ENTITYSTATUS) AS ENTITY_STATUS,
     TRY_CAST(RECORDNO AS INT) AS RECORD_NO,
-
     -- Audit
     _FIVETRAN_DELETED AS IS_DELETED,
     CURRENT_TIMESTAMP()::TIMESTAMP_NTZ AS SILVER_LOAD_DATE
 
 FROM {{ source('spotless_sage', 'ENTITYACCTDEFAULT') }}
 
-{% if is_incremental() %}
-WHERE RECORDNO > (
-    SELECT COALESCE(MAX(RECORD_NO), 0)
-    FROM {{ this }}
-)
-{% endif %}
