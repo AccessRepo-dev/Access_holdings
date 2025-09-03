8534eb791ed013e17bde0b9234f86e45df33b961
srinivas.annem@accordion.com <srinivas.annem@accordion.com>
2025-09-03 09:42:53 +0000

    removed alias

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -1,8 +1,17 @@
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+/*
+{% macro generate_alias_name1(custom_alias_name=none, node=none) -%}
   {% set parent_folder = node.path.split('/')[-2] %}
   {% if custom_alias_name is not none %}
     {{ parent_folder }}_{{ custom_alias_name }}
   {% else %}
     {{ parent_folder }}_{{ node.name }}
+  {% endif %}
+{%- endmacro %}
+*/
+{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+  {% if custom_alias_name is not none %}
+    {{ custom_alias_name }}
+  {% else %}
+    {{ node.name }}
   {% endif %}
 {%- endmacro %}
