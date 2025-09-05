fd9132c96ff2031edf6daa857d18591a5180d101
Prasad Bale <prasad.bale@accordion.com>
2025-09-05 12:00:38 +0000

    created dims for currency and currency rate

---
+++ models/integrations/gold/netsuite/dim_currency.sql
@@ -0,0 +1,29 @@
+{% set company = var('company', 'Unknown company') | lower %}
+{{ config(enabled = var('sourcesystem', 'none') == 'netsuite') }}
+
+{{ config(
+    database = get_target_database(company),
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'CURRENCY_ID'
+) }}
+
+with source as (
+    select 
+    ID AS CURRENCY_ID,
+    NAME AS CURRENCY_NAME,
+    SYMBOL AS DISPLAY_SYMBOL,
+    ISINACTIVE AS IS_INACTIVE,
+    ISBASECURRENCY AS IS_BASE_CURRENCY,
+    LASTMODIFIEDDATE AS LAST_MODIFIED_DATE,
+from {{ get_silver_source(company, 'netsuite_currency') }}
+    where (_fivetran_deleted is null or _fivetran_deleted = false)
+    {% if is_incremental() %}
+    and LASTMODIFIEDDATE > (
+        select coalesce(max(LAST_MODIFIED_DATE), '1900-01-01')
+        from {{ this }}
+    )
+    {% endif %}
+)
+select *
+from source
