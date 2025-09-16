0c1f3f9aab26ab222816108ed97dc1ecd395aa73
Prasad Bale <prasad.bale@accordion.com>
2025-09-16 06:20:45 +0000

    Created Budget header and Item for Sage in Silver Layer

---
+++ models/integrations/gold/sage/sage_dim_budget_header.sql
@@ -0,0 +1,28 @@
+{% set company = var('company', 'Unknown company') | lower %}
+{{ config(enabled = var('sourcesystem', 'none') == 'sage') }}
+
+{{ config(
+    database = get_target_database(company),
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'DIM_BUDGET_CATEGORY_ID'
+) }}
+
+with source as (
+    select
+        RECORDNO AS DIM_BUDGET_CATEGORY_ID,
+        BUDGETID,
+        DESCRIPTION,
+        STATUS AS IS_INACTIVE,
+        WHENMODIFIED AS LAST_MODIFIED_DATE
+    from {{ get_silver_source(company, 'sage_gl_budget_header') }}
+    
+    {% if is_incremental() %}
+    where WHENMODIFIED > (
+        select coalesce(max(LAST_MODIFIED_DATE), '1900-01-01')
+        from {{ this }}
+    )
+    {% endif %}
+)
+select *
+from source
