a5bbdbdcb533d137d0c9914b875a23fc8d62b047
Prasad Bale <prasad.bale@accordion.com>
2025-09-17 06:20:07 +0000

    Sage Gold Transaction Changes

--- models/integrations/gold/sage/sage_fact_transaction.sql
+++ models/integrations/gold/sage/sage_fact_transaction.sql
@@ -1,52 +1,82 @@
-
 {% set company = var('company', 'Unknown company') | lower %}
 {{ config(enabled = var('sourcesystem', 'none') == 'sage') }}
-
+ 
 {{ config(
     database = get_target_database(company),
     materialized = 'table',
-    unique_key = 'TRANSACTION_LINE_ID'
+    unique_key = 'TRANSACTIONS_UNIQUE_ID'
 ) }}
-
-with source as (
-    SELECT 
-    e.RECORDNO AS TRANSACTION_LINE_ID,
-    d.RECORDNO AS TRANSACTIONS_UNIQUE_ID,
-    b.RECORDNO AS TRAN_ID,
-    
-    d.BATCHKEY AS TRANSACTION_ID,
-    e.BATCHNO AS TRANSACTION_NUMBER,
-    e.LINE_NO AS TRANSACTION_LINE_NO,
-    d.LINE_NO AS ACCOUNTING_LINE_NO,
-    l.ENTITY AS SUBSIDIARY_ID,
-
-    e.TR_TYPE AS TRANSACTION_TYPE,
-    e.BATCHTITLE AS TITLE,
-    b.BATCH_TITLE AS MEMO,
-    e.STATE AS STATE,
-
-    e.ENTRY_DATE AS TRAN_DATE,
-    b.BATCH_DATE AS REPORTING_PERIOD_ID,
-    e.BATCH_DATE AS POSTING_PERIOD_DATE,
-
-    e.ACCOUNTKEY AS ACCOUNT_ID,
-    e.CLASSID AS CLASS_ID,
-    ABS(HASH(e.ACCOUNTKEY,l.ENTITY)) AS CHART_OF_ACCOUNTS_UNIQUE_ID,
-    e.ITEMDIMKEY as ITEM_ID,
-    e.CURRENCY AS CURRENCY_ID,
-    e.LOCATIONKEY AS LOCATION_ID,
-    e.LOCATIONNAME AS LOCATION_NAME,
-    e.DEPARTMENTKEY AS DEPARTMENT_ID,
  
-    e.TRX_AMOUNT AS NET_AMOUNT,
+with source as (
+    SELECT
+    -- Identifiers
+    CONCAT(CAST(d.RECORDNO AS VARCHAR), '-', CAST(e.RECORDNO AS VARCHAR)) AS TRANSACTIONS_UNIQUE_ID,
+    d.BATCHKEY AS TRANSACTION_ID,
+    e.RECORDNO AS TRANSACTION_LINE_ID,
+    e.BATCHNO AS TRANSACTION_NUMBER,
+    e.BATCHTITLE AS TRANID,
+    e.TR_TYPE AS TRANSACTION_TYPE,
+    e.STATE AS STATUS,
+    e.BATCHTITLE AS TITLE,
+    e.STATE AS STATUS_NAME,  
+ 
+    -- Accounts
+    e.ACCOUNTKEY AS ACCOUNT_ID,
+    acc.ACCOUNTNO AS ACCOUNT_NUMBER,
+    acc.ACCOUNTTYPE AS ACCOUNT_TYPE,
+    acc.TITLE AS ACCOUNT_NAME,
+    abs(HASH(e.ACCOUNTKEY,e.LOCATIONKEY)) AS DIM_CHART_OF_ACCOUNT_ID,
+    e.CLASSID AS DIM_CLASS_ID,
+ 
+    -- Transaction Line
+    e.ITEMDIMKEY AS DIM_ITEM_ID,
+    e.CLASSID AS CLASS ,
+    e.DEPARTMENTKEY AS DIM_DEPARTMENT_ID,
+    NULL AS DIM_ENTITY_ID,
+    --NULL AS ITEM_TYPE,
+    NULL AS TRANSACTION_LINE_TYPE,
+    d.LINE_NO AS ACCOUNTING_LINE_TYPE,
+    e.LOCATIONKEY AS DIM_LOCATION_ID,
+    e.LOCATIONKEY AS DIM_SUBSIDIARY_ID,
+    TRUE AS IS_POSTING,
+ 
+    -- Period / Currency
+    b.BATCH_DATE AS POSTINGPERIOD,
+    e.BATCH_DATE AS POSTING_PERIOD_DATE,
+    e.CURRENCY AS CURRENCY,
+    CONCAT(e.LOCATIONKEY, '-', b.BATCH_DATE, '-', e.CURRENCY) AS CONSOLIDATED_EXCHANGE_RATE_UNIQUE_ID,
+    NULL AS EXCHANGERATE,
+ 
+    -- Amounts
+    e.TRX_AMOUNT AS NETAMOUNT,
     e.AMOUNT AS AMOUNT,
-    e.WHENMODIFIED AS LAST_MODIFIED_DATE
-    FROM {{ get_silver_source(company, 'sage_gl_entry') }}  e 
-    LEFT JOIN  {{ get_silver_source(company, 'sage_gl_detail') }} d on d.GLENTRYKEY = e.recordno
-    LEFT JOIN {{ get_silver_source(company, 'sage_gl_batch') }}  b on d.batchkey = b.recordno 
-    LEFT JOIN {{ get_silver_source(company, 'sage_location') }}  l on l.LOCATION_ID = e.LOCATIONKEY
-    LEFT JOIN {{ get_silver_source(company, 'sage_coa_mapping') }}  map on ABS(HASH(e.ACCOUNTKEY,l.LOCATIONKEY))= ABS(HASH(map.ACCOUNT_ID,map.LOCATION_ID))
-
+    e.TRX_AMOUNT AS CONVERTED_NET_AMOUNT,
+    NULL AS BOM_QUANTITY,
+    NULL AS QUANTITY,
+ 
+    -- Employee / Customer / Address
+    NULL AS EMPLOYEE,
+    NULL AS BILLINGADDRESS,
+    NULL AS SHIPPINGADDRESS,
+    NULL AS BILLINGSTATUS,
+    b.BATCH_TITLE AS MEMO,
+ 
+    -- Dates
+    e.ENTRY_DATE AS TRANDATE,
+    NULL AS STARTDATE,
+    NULL AS ENDDATE,
+    NULL AS DUEDATE,
+    NULL AS CLOSEDATE,
+    e.WHENMODIFIED AS LASTMODIFIEDDATE
+ 
+FROM {{ get_silver_source(company, 'sage_gl_entry') }}  e
+LEFT JOIN {{ get_silver_source(company, 'sage_gl_detail') }} d
+    ON d.GLENTRYKEY = e.recordno
+LEFT JOIN {{ get_silver_source(company, 'sage_gl_batch') }}  b
+    ON d.batchkey = b.recordno
+LEFT JOIN {{ get_silver_source(company, 'sage_gl_account') }} acc
+    ON e.ACCOUNTKEY  = acc.RECORDNO
+ 
 )
 SELECT *
 FROM source
