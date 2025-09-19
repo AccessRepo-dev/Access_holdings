c5556122e87d1d7484a547e70760673eeccd30a7
Lahari Yadav <lahari.yadav@accordion.com>
2025-09-19 07:57:55 +0000

    updated consolidated

--- models/consolidated/gold/consolidated_dim_period.sql
+++ models/consolidated/gold/consolidated_dim_period.sql
@@ -1,20 +1,21 @@
 {{ config(
     materialized = 'incremental',
+    alias = 'dim_period',
     incremental_strategy = 'merge',
-    unique_key = ['DIM_PERIOD_ID','SOURCESYSTEM','COMPANY']
+    unique_key = ['DIM_PERIOD_ID']
 ) }}
 
 
 {% set companies = [
-    {"name": "WAGWAY",   "db": env_var('DBT_WAGWAY', 'wagway_dev'),   "schema": "gold", "table": "NETSUITE_DIM_PERIOD", "source": "NETSUITE"},
-    {"name": "PLAYFLY",  "db": env_var('DBT_PLAYFLY', 'playfly_dev'), "schema": "gold", "table": "NETSUITE_DIM_PERIOD", "source": "NETSUITE"},
-    {"name": "SPOTLESS", "db": env_var('DBT_SPOTLESS', 'spotless_dev'), "schema": "gold", "table": "SAGE_DIM_PERIOD", "source": "SAGE"},
-    {"name": "AMH",      "db": env_var('DBT_AMH', 'amh_dev'),         "schema": "gold", "table": "SAGE_DIM_PERIOD", "source": "SAGE"}
+    {"name": "WAGWAY",   "db": env_var('DBT_WAGWAY'), "source": "NETSUITE"},
+    {"name": "PLAYFLY",  "db": env_var('DBT_PLAYFLY'),  "source": "NETSUITE"},
+    {"name": "SPOTLESS", "db": env_var('DBT_SPOTLESS'), "source": "SAGE"},
+    {"name": "AMH",      "db": env_var('DBT_AMH'), "source": "SAGE"}
 ] %}
 
 {% for c in companies %}
     select
-        HASH(DIM_PERIOD_ID, '{{ c.name }}') AS DIM_PERIOD_ID,
+        HASH(DIM_PERIOD_ID, '{{ c.name }}','{{ c.source }}') AS DIM_PERIOD_ID,
         DIM_PERIOD_ID AS PERIOD_ID,
         PERIOD_NAME,
         START_DATE,
@@ -25,7 +26,7 @@
         '{{ c.source }}' AS SOURCESYSTEM,
         '{{ c.name }}' AS COMPANY,
         CURRENT_TIMESTAMP()::TIMESTAMP_NTZ AS CONSOLIDATED_GOLD_LOAD_DATE
-    from {{ c.db }}.{{ c.schema }}.{{ c.table }}
+    from {{ c.db }}.GOLD.DIM_PERIOD
 
     {% if is_incremental() %}
     and LAST_MODIFIED_DATE > (
--- models/consolidated/gold/consolidated_fact_transaction.sql
+++ models/consolidated/gold/consolidated_fact_transaction.sql
@@ -1,21 +1,24 @@
 {{ config(
     materialized = 'incremental',
+    alias = 'fact_transaction',
     incremental_strategy = 'merge',
-    unique_key = ['TRANSACTIONS_UNIQUE_ID','SOURCESYSTEM','COMPANY']
+    unique_key = ['FACT_TRANSACTION_ID',]
 ) }}
 
 {% set companies = [
-    {"name": "WAGWAY",   "db": env_var('DBT_WAGWAY', 'wagway_dev'),   "schema": "gold", "table": "NETSUITE_FACT_TRANSACTION", "source": "NETSUITE"},
+    {"name": "WAGWAY",   "db": env_var('DBT_WAGWAY'), "source": "NETSUITE"},
+    {"name": "PLAYFLY",  "db": env_var('DBT_PLAYFLY'),  "source": "NETSUITE"},
+    {"name": "SPOTLESS", "db": env_var('DBT_SPOTLESS'), "source": "SAGE"},
+    {"name": "AMH",      "db": env_var('DBT_AMH'),   "source": "SAGE"}
 
-    {"name": "SPOTLESS", "db": env_var('DBT_SPOTLESS', 'spotless_dev'), "schema": "gold", "table": "SAGE_FACT_TRANSACTION", "source": "SAGE"}
  
 ] %}
 
 {% for c in companies %}
     select
-        HASH(TRANSACTIONS_UNIQUE_ID, '{{ c.name }}') as FACT_TRANSACTION_ID,
+        HASH(TRANSACTIONS_UNIQUE_ID, '{{ c.name }}','{{ c.source }}') as FACT_TRANSACTION_ID,
         TRANSACTIONS_UNIQUE_ID ,
-        CAST(TRANSACTION_ID AS INT) ,
+        CAST(TRANSACTION_ID AS INT) AS TRANSACTION_ID ,
         TRANSACTION_LINE_ID,
         TRANSACTION_NUMBER,
         TRANID,
@@ -24,25 +27,25 @@
         TITLE,
         STATUS_NAME,
         ACCOUNT_ID,
-        CAST(ACCOUNT_NUMBER AS VARCHAR) ,
+        CAST(ACCOUNT_NUMBER AS VARCHAR) AS ACCOUNT_NUMBER ,
         ACCOUNT_TYPE,
         ACCOUNT_NAME,
-        HASH(DIM_CHART_OF_ACCOUNT_ID,'{{ c.name }}') AS  DIM_CHART_OF_ACCOUNT_ID,
-        HASH(DIM_CLASS_ID,'{{ c.name }}') AS DIM_CLASS_ID,
+        HASH(DIM_CHART_OF_ACCOUNT_ID,'{{ c.name }}','{{ c.source }}') AS  DIM_CHART_OF_ACCOUNT_ID,
+        HASH(DIM_CLASS_ID,'{{ c.name }}','{{ c.source }}') AS DIM_CLASS_ID,
         METRIC_L1,
         METRIC_L2,
         METRIC_L3,
         METRIC_L4,
         METRIC_L5,
         METRIC_L6,
-        HASH(DIM_ITEM_ID,'{{ c.name }}') AS  DIM_ITEM_ID,
+        HASH(DIM_ITEM_ID,'{{ c.name }}','{{ c.source }}') AS  DIM_ITEM_ID,
         CLASS,
-        HASH(DIM_DEPARTMENT_ID,'{{ c.name }}') AS DIM_DEPARTMENT_ID,
+        HASH(DIM_DEPARTMENT_ID,'{{ c.name }}','{{ c.source }}') AS DIM_DEPARTMENT_ID,
         DIM_ENTITY_ID,
         TRANSACTION_LINE_TYPE,
         ACCOUNTING_LINE_TYPE,
-        HASH(DIM_LOCATION_ID,'{{ c.name }}') AS DIM_LOCATION_ID,
-        HASH(DIM_SUBSIDIARY_ID,'{{ c.name }}') AS DIM_SUBSIDIARY_ID,
+        HASH(DIM_LOCATION_ID,'{{ c.name }}','{{ c.source }}') AS DIM_LOCATION_ID,
+        HASH(DIM_SUBSIDIARY_ID,'{{ c.name }}','{{ c.source }}') AS DIM_SUBSIDIARY_ID,
         IS_POSTING,
         POSTINGPERIOD,
         POSTING_PERIOD_DATE,
@@ -68,7 +71,7 @@
         '{{ c.source }}' as SOURCESYSTEM,
         '{{ c.name }}' as COMPANY,
         current_timestamp()::timestamp_ntz as CONSOLIDATED_GOLD_LOAD_DATE
-    from {{ c.db }}.{{ c.schema }}.{{ c.table }}
+    from {{ c.db }}.GOLD.FACT_TRANSACTION
 
     {% if is_incremental() %}
     where LASTMODIFIEDDATE > (
