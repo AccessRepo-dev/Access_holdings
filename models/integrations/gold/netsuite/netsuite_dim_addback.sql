1bc7e8c5fc0598d0d383c2957edf935b813685dc
Lahari Yadav <lahari.yadav@accordion.com>
2025-09-25 04:51:34 +0000

    added customrecord_cseg1 (dim_addback)

---
+++ models/integrations/gold/netsuite/netsuite_dim_addback.sql
@@ -0,0 +1,30 @@
+{% set company = var('company', 'Unknown company') | lower %}
+{{ config(enabled = var('sourcesystem', 'none') == 'netsuite') }}
+
+{{ config(
+    database = get_target_database(company),
+    materialized = 'incremental',
+    alias = 'dim_addback',
+    incremental_strategy = 'merge',
+    unique_key = 'DIM_ADDBACK_ID'
+) }}
+
+with source as (
+
+    select
+        ID as DIM_ADDBACK_ID,
+        NAME,
+        ISINACTIVE AS IS_INACTIVE,
+        LASTMODIFIED as LAST_MODIFIED_DATE
+    from {{ get_silver_source(company, 'CUSTOMRECORD_CSEG1') }}
+    where (_fivetran_deleted is null or _fivetran_deleted = false)
+
+    {% if is_incremental() %}
+        and LASTMODIFIED > (
+            select coalesce(max(LAST_MODIFIED_DATE), '1900-01-01')
+            from {{ this }}
+        )
+    {% endif %}
+)
+select *
+from source
