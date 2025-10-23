2805444340d8a8c7dfe773fe97f869c3a2aa526f
Lahari Yadav <lahari.yadav@accordion.com>
2025-10-23 16:01:47 +0000

    updated macro silver

--- models/integrations/silver/macro/nahb_housing.sql
+++ models/integrations/silver/macro/nahb_housing.sql
@@ -1,11 +1,17 @@
 with source as (
     select *
     from {{ source('macro_raw', 'NAHB_HOUSING') }}
-)
-
-select
+),
+transformed AS (
+    select
     TRIM("month") AS MONTH,
     CAST(TRIM(SPLIT_PART("month",',',2)) AS NUMBER) AS YEAR,
     TRIM(SPLIT_PART("month",' ',1)) AS MONTH_NAME,
     "tr_td" AS HMI --HOUSING MARKET INDEX
 from source
+)
+SELECT 
+*,
+YEAR * 100 + EXTRACT(MONTH FROM TO_DATE(month_name, 'MMMM')) AS DateKey
+FROM transformed
+
