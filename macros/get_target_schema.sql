3c195e7d00796c7bb8eed0dd5e18f6aa0760a650
Prasad Bale <prasad.bale@accordion.com>
2025-10-31 08:02:44 +0000

    Created snapshot config files for hubspot and salesforce

---
+++ macros/get_target_schema.sql
@@ -0,0 +1,5 @@
+{% macro target_snapshot_schema(sourcesystem) -%}
+  {% set sourcesystem = sourcesystem | lower %}
+  {% set schema_name = sourcesystem | lower ~ '_snapshots' %}
+  {{ return(schema_name) }}
+{%- endmacro %}
