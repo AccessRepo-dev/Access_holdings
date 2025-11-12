53727fbe4465dbd7417c105e1c83adf9d5b5808b
Prasad Bale <prasad.bale@accordion.com>
2025-11-12 11:40:45 +0000

    Created snapshot tables for hubspot

--- models/integrations/silver/hubspot/hubspot_quote.sql
+++ models/integrations/silver/hubspot/hubspot_quote.sql
@@ -1,33 +1,39 @@
-{% set company = var('company') %} 
+{% set company = var('company') %} --wagway , playfly
 {% set sourcesystem = var('sourcesystem') %}
 {{ config(enabled = var('sourcesystem', 'none') in ['hubspot']) }}
-{{ config(enabled = var('company', 'none') in ['AMH']) }}
-
+{{ config(enabled = var('company', 'none') == ['AMH']) }}
 
 
 {{ config(
     
     database=get_target_database(var('company')),
     materialized = 'incremental',
-    alias =  sourcesystem ~ '_QUOTE',
+    alias =  sourcesystem ~ '_LEAD',
     incremental_strategy = 'merge',
-    unique_key = 'QUOTE_ID'
+    unique_key = 'HS_LEAD_ID'
 ) }}
 
-SELECT
-    ID AS QUOTE_ID,   
-    INITCAP(TRIM(PROPERTY_HS_SENDER_COMPANY_NAME)) AS PROPERTY_HS_SENDER_COMPANY_NAME,
-    PROPERTY_HS_QUOTE_AMOUNT,
-    TRIM(PROPERTY_HS_QUOTE_PROGRESSION_STATUS) AS PROPERTY_HS_QUOTE_PROGRESSION_STATUS,
+select
+    ID AS HS_LEAD_ID,
+    CAST(TRIM(PROPERTY_HUBSPOT_OWNER_ID) AS INT) AS PROPERTY_HUBSPOT_OWNER_ID,
+    
+    INITCAP(TRIM(PROPERTY_HS_ASSOCIATED_CONTACT_FIRSTNAME)) AS PROPERTY_HS_ASSOCIATED_CONTACT_FIRSTNAME,
+    INITCAP(TRIM(PROPERTY_HS_ASSOCIATED_CONTACT_LASTNAME)) AS PROPERTY_HS_ASSOCIATED_CONTACT_LASTNAME,
+    
+    LOWER(TRIM(PROPERTY_HS_ASSOCIATED_CONTACT_EMAIL)) AS PROPERTY_HS_ASSOCIATED_CONTACT_EMAIL,
+    
+    UPPER(TRIM(PROPERTY_HS_LEAD_SOURCE)) AS PROPERTY_HS_LEAD_SOURCE,
+    
     TRY_TO_TIMESTAMP_NTZ(TRIM(PROPERTY_HS_CREATEDATE)) AS PROPERTY_HS_CREATEDATE,
     TRY_TO_TIMESTAMP_NTZ(TRIM(PROPERTY_HS_LASTMODIFIEDDATE)) AS PROPERTY_HS_LASTMODIFIEDDATE,
     _FIVETRAN_SYNCED
 
-from {{ get_raw_source(company, sourcesystem, 'QUOTE') }}
+    
+from {{ get_raw_source(company, sourcesystem, 'LEAD') }}
     {% if is_incremental() %}
     where PROPERTY_HS_LASTMODIFIEDDATE > (
         select coalesce(max(PROPERTY_HS_LASTMODIFIEDDATE), '1900-01-01'::timestamp_ntz)
         from {{ this }}
     )
-   -- or _FIVETRAN_DELETED = true
+ 
     {% endif %}
