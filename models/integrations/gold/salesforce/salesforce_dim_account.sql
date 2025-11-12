8052d6f52319459cbd16b588cb2ef478c1e0abf5
Lahari Yadav <lahari.yadav@accordion.com>
2025-11-12 05:48:01 +0000

    scd2

--- models/integrations/gold/salesforce/salesforce_dim_account.sql
+++ models/integrations/gold/salesforce/salesforce_dim_account.sql
@@ -26,7 +26,7 @@ with source as (
         LAST_MODIFIED_DATE,
         DBT_VALID_FROM,
         DBT_VALID_TO,
-        CASE WHEN dbt_valid_to IS NULL THEN 1 ELSE 0 END AS Is_Active,
+        CASE WHEN DBT_VALID_TO IS NULL THEN 1 ELSE 0 END AS Is_Active,
         CURRENT_TIMESTAMP()::TIMESTAMP_NTZ AS GOLD_LOAD_DATE
     from {{ get_silver_source(company, 'SALESFORCE_ACCOUNT') }}
 
---
+++ models/integrations/silver/macro/bls_unemployment.sql
@@ -0,0 +1,14 @@
+with source as (
+    select *
+    from {{ source('macro_raw', 'BLS_UNEMPLOYMENT') }}
+)
+
+select
+    TRIM("series_id") as SERIES_ID,
+    CAST("year" AS NUMBER) as YEAR,
+    TRIM("period") as PERIOD,
+    CAST(CONCAT(YEAR,SUBSTRING("period", 2, 2),'01') AS INT) AS DateKey,
+    "value" as VALUE,
+    TRIM("series_title") as SERIES_TITLE,
+    "periodicity_code" as periodicity_code
+from source
