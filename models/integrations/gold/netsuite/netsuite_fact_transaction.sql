e91db1b6d17497045197c7591347017537fbc999
Lahari Yadav <lahari.yadav@accordion.com>
2025-12-08 09:45:04 +0000

    added config columns

--- models/integrations/gold/netsuite/netsuite_fact_transaction.sql
+++ models/integrations/gold/netsuite/netsuite_fact_transaction.sql
@@ -196,7 +196,7 @@ SELECT * FROM source
             COALESCE(lower(STATUS_NAME), '') <> 'rejected'
             AND IS_POSTING = TRUE
             AND NOT  (ACCOUNT_NUMBER = 50100 AND  MEMO IN ('To Reclass Q1 FY26 Rights Fees Expenses to New Expense Accounts','To Reclass Q1 FY26 Rights Fees Expense to New Expense Accounts') ) 
-            AND NOT (DIM_PERIOD_ID = 105  )
+          
         
            
 {% endif %}
