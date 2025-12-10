95dcd542be18578626e093e07f8e675538f184fc
Lahari Yadav <lahari.yadav@accordion.com>
2025-12-10 06:44:25 +0000

    updated budget grouped_header

--- models/integrations/gold/netsuite/netsuite_dim_budget_header.sql
+++ models/integrations/gold/netsuite/netsuite_dim_budget_header.sql
@@ -11,6 +11,14 @@ with source as (
         ID AS DIM_BUDGET_HEADER_ID,
         BUDGETTYPE AS BUDGET_TYPE,
         NAME,
+        {% if company == 'playfly'%}
+        CASE 
+            WHEN NAME = 'SOH Canada'
+            THEN 'Budget' ELSE  NAME 
+        END AS GROUPED_HEADER 
+        {%else%}
+        NAME AS GROUPED_HEADER 
+        {%endif%}
         ISINACTIVE AS IS_INACTIVE
          
     from {{ref('netsuite_budgetcategory')}}
