571588e23b3453d62aeb50ed150dcd6f99f25c21
Lahari Yadav <lahari.yadav@accordion.com>
2025-12-24 09:54:37 +0000

    updated dim_period_flag in zeus

---
+++ models/integrations/gold/sedona/sedona_dim_period_flag.sql
@@ -0,0 +1,76 @@
+{% set company = var('company', 'zeus') | lower %}
+{{ config(enabled = var('company', 'zeus') in ['zeus'] ) }}
+{{ config(enabled = var('sourcesystem', 'sedona') == 'sedona') }}
+
+{{ config(
+    database = get_target_database(company),
+    alias = 'dim_period_flag',
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['DIM_PERIOD_ID','START_DATE','FLAG_TYPE']
+) }}
+
+with source as (
+    SELECT 
+        DIM_PERIOD_ID,
+        START_DATE
+    FROM  {{ get_gold_source(company, 'dim_period') }} 
+   WHERE
+    START_DATE <= CURRENT_DATE
+),
+
+base AS (
+    SELECT 
+        DIM_PERIOD_ID,
+        START_DATE,
+
+        CASE 
+            WHEN YEAR(START_DATE) = YEAR(CURRENT_DATE) 
+            THEN 'YTD' 
+        END AS YTD_FLAG,
+
+        CASE 
+            WHEN YEAR(START_DATE) = YEAR(CURRENT_DATE)
+                 AND MONTH(START_DATE) = MONTH(CURRENT_DATE)
+            THEN 'MTD' 
+        END AS MTD_FLAG,
+
+        CASE 
+            WHEN YEAR(START_DATE) = YEAR(CURRENT_DATE)
+                 AND QUARTER(START_DATE) = QUARTER(CURRENT_DATE)
+            THEN 'QTD' 
+        END AS QTD_FLAG,
+
+        CASE 
+            WHEN START_DATE BETWEEN 
+                 dateadd(month, -12, date_trunc('month', current_date))
+                 and date_trunc('month', dateadd(month, -1, current_date))
+            THEN 'LTM' 
+        END AS LTM_FLAG
+    FROM source
+),
+
+unpivoted AS (
+    SELECT 
+        DIM_PERIOD_ID,
+        START_DATE,
+        FLAG_TYPE
+    FROM base
+    UNPIVOT (
+        FLAG_TYPE FOR FLAG_COL IN (YTD_FLAG, MTD_FLAG, QTD_FLAG, LTM_FLAG)
+    )
+),
+
+final AS (
+    SELECT
+        b.DIM_PERIOD_ID,
+        b.START_DATE,
+        COALESCE(u.FLAG_TYPE, 'HISTORICAL') AS FLAG_TYPE
+    FROM base b
+    LEFT JOIN unpivoted u
+        ON b.DIM_PERIOD_ID = u.DIM_PERIOD_ID
+)
+
+SELECT *
+FROM final
+ORDER BY START_DATE
