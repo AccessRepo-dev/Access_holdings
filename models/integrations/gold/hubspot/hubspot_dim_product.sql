652d96f2ed49670916cb104afa800d047cd95bbb
Vaishnavi Siddaveni <vaishnavi.siddaveni@accordion.com>
2026-01-12 10:34:34 +0000

    updated dim product

--- models/integrations/gold/hubspot/hubspot_dim_product.sql
+++ models/integrations/gold/hubspot/hubspot_dim_product.sql
@@ -9,19 +9,19 @@
 ) }}
 
 SELECT
-    id_date_key,
-    product_id,
-    product_name,
-    property_description,
-    property_family,
-    portal_id,
-    pricing_model,
-    product_status.
-    product_type,
-    property_hs_folder_id,
-    price,
-    created_at,
-    is_deleted,
+    p.id_date_key,
+    p.product_id,
+    p.product_name,
+    p.property_description,
+    p.property_family,
+    p.portal_id,
+    p.pricing_model,
+    p.product_status,
+    p.product_type,
+    p.property_hs_folder_id,
+    p.price,
+    p.created_at,
+    p.is_deleted,
     b.DEAL_ID AS OPPORTUNITY_ID
 
 FROM {{ get_silver_source(company , 'HUBSPOT_PRODUCT') }} p
