808be7ab14e1fdd32f00a927e8ce34957f9874ab
Vaishnavi Siddaveni <vaishnavi.siddaveni@accordion.com>
2026-02-12 13:02:51 +0000

    updated macro parameters in coa_mapping

--- models/integrations/streamlit_staging/netsuite/netsuite_streamlit_coa.sql
+++ models/integrations/streamlit_staging/netsuite/netsuite_streamlit_coa.sql
@@ -9,7 +9,7 @@
     enabled = var('sourcesystem') == 'netsuite',
     pre_hook=[
             "{{ replicate_table_from_dev(
-                source_database= this.database,
+                source_database= 'STREAMLIT_APPS',
                 source_schema='FINMAP_DEV',
                 table_name = var('company') ~ '_COA_MAPPING'
             ) }}"
